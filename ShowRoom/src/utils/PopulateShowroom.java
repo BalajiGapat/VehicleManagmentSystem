86118ea113de73cd5d07d01a11eb3254a1011696
Balaji Gapat <gapatbalaji2901@gmail.com>
2024-04-15 15:21:03 +0530

    b3

--- ShowRoom/src/utils/PopulateShowroom.java
+++ ShowRoom/src/utils/PopulateShowroom.java
@@ -17,12 +17,12 @@ public  class PopulateShowroom
 	{	
 		List<Vehicle> vList=new ArrayList<>();
 
-		vList.add(validateAllInputs("ABC-101", "black", "2023-02-26", 500000, "Honda", vList));
-		vList.add(validateAllInputs("ABC-106", "white", "2023-04-12", 900000, "Honda", vList));
-		vList.add(validateAllInputs("ABC-105", "white", "2023-04-12", 800000, "Honda", vList));
-		vList.add(validateAllInputs("ABC-104", "red", "2023-03-16", 400000, "Honda", vList));
-		vList.add(validateAllInputs("ABC-103", "black", "2023-05-03", 300000, "Honda", vList));
-		vList.add(validateAllInputs("ABC-102", "blue", "2023-12-29", 700000, "Honda", vList));
+		vList.add(validateAllInputs("ABC-101", "black", "2024-02-26", 500000, "Honda", vList));
+		vList.add(validateAllInputs("ABC-106", "white", "2024-04-12", 900000, "Honda", vList));
+		vList.add(validateAllInputs("ABC-105", "white", "2024-04-12", 800000, "Honda", vList));
+		vList.add(validateAllInputs("ABC-104", "red", "2024-03-16", 400000, "Honda", vList));
+		vList.add(validateAllInputs("ABC-103", "black", "2024-05-03", 300000, "Honda", vList));
+		vList.add(validateAllInputs("ABC-102", "blue", "2024-12-29", 700000, "Honda", vList));
 		return vList;
 	}
 	
